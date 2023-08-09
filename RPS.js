ed120db0b346979c783a6ce5271428456db944e8
HYSA <aj.anunciado@gmail.com>
2023-08-09 20:56:48 +0800

    made some changes

--- RPS.js
+++ RPS.js
@@ -1,26 +1,45 @@
 let btn= document.getElementById("button-el")
 let screen= document.getElementById("element-El")
-let rock
-let paper
-let gunting
 
-
+// let cards={
+//   rock:"rock",
+//   paper:"paper",
+//   scissors:"scissors"
+// }
+const cards = {rock: "✊", paper: "🤚", scissors: "✌"};
+// let cards = [
+//   {
+//     key: "rock",
+//     value: "rock"
+//   },
+//   {
+//     key: "paper",
+//     value: "paper"
+//   }
+// ];
+console.log(cards.rock)
  function startGame(){
  
 
  }
 const random = () =>{
-let modes= Math.floor(Math.random()*3)+1
-return [rock,paper,gunting] [modes]
+let index= Math.floor(Math.random()*3)
+let index1= Math.floor(Math.random()*3)
+let player=cards[index]
+let computer=cards[index1]
+return[player,computer]
 }
+screen.addEventListener("click",function(){
 let result= random()
-
+console.log(result)
+screen.textContent=result[0] + " VS "+ result[1];
+})
 const logic = () =>{
-  if(rock==="rock" && gunting==="gunting"){
+  if(cards.rock=== "✊" && cards.scissors==="✌"){
   console.log("Rock Wins!!!")
-}else if(gunting==="gunting" && paper==="paper" ){
+}else if(cards.scissors==="✌" && cards.paper==="🤚" ){
 console.log("Gunting wins!!!")
-}else if(paper==="paper" && rock==="rock"){
+}else if(cards.paper==="🤚" && cards.rock==="✊"){
   console.log("Paper wins!!!")
 }
 }
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,24 +1,6 @@
-let rock
-let paper
-let gunting
-
-
- function startGame(){
- 
-
- }
-const random = () =>{
-let modes= Math.floor(Math.random()*3)+1
-return [rock,paper,gunting] [modes]
+let cards={
+  rock:"rock",
+  paper:"paper",
+  scissors:"scissors"
 }
-let result= random()
-
-const logic = () =>{
-  if(rock==="rock" && gunting==="gunting"){
-  console.log("Rock Wins!!!")
-}else if(gunting==="gunting" && paper==="paper" ){
-console.log("Gunting wins!!!")
-}else if(paper==="paper" && rock==="rock"){
-  console.log("Paper wins!!!")
-}
-}
+console.log(cards.rock)
